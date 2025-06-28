e94349017d19bbe90dc15c2d7ee7b3e05095eabd
Shahd <you@example.com>
2025-06-28 15:22:35 +0300

    header

---
+++ public/images/page.ts
@@ -0,0 +1,4 @@
+import logo from './logo.png'
+import BookInnLogo from './BookInn-logo.png'
+
+export {logo, BookInnLogo}
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { ThemeToggle } from '@/components/ui/theme-toggle'
+
+const page = () => {
+  return (
+    <main>
+      {/* <ThemeToggle /> */}
+    </main>
+  )
+}
+
+export default page
