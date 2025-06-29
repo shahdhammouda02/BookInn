4295ee8021ffb6fc9a5f361261739a98ead3f80e
Shahd <you@example.com>
2025-06-29 22:16:19 +0300

    solve-hydraitionsptoblems

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -22,7 +22,7 @@ export function ThemeToggle() {
         <Button variant="none" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">{t("Toggle theme")}</span>
+          <span className="sr-only">{t("buttons.themeToggle")}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
