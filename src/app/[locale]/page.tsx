c773754798e073279c49b447669f005369ddd677
Shahd <you@example.com>
2025-07-03 20:15:59 +0300

    cards-update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import {Home} from '@/features/page'
-import { hotelsData } from '@/data/cardsData'
+import React from "react";
+import { Home } from "@/features/page";
+import { hotelsData } from "@/data/cardsData";
 
 const page = () => {
-  
   return (
     <main>
-      <Home hotels={hotelsData}/>
+      <Home hotels={hotelsData} />
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
