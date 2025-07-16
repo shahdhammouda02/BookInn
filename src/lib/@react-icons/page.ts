04c6b472fe1fdd728503a0c6e401821215ae376a
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2025-07-16 17:34:03 +0300

    signIn-auth

--- src/lib/@react-icons/page.ts
+++ src/lib/@react-icons/page.ts
@@ -2,5 +2,6 @@ import { BiRestaurant as Restaurant } from "react-icons/bi";
 import { MdEvent as Event } from "react-icons/md";
 import { IoFitness as Fitness } from "react-icons/io5";
 import { FaServicestack as Service} from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
-export {Restaurant, Event, Fitness, Service}
+export {Restaurant, Event, Fitness, Service, FaGoogle}
