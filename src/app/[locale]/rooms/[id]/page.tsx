1cd1b1476d91dde935a54b746220f1757647ded1
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2025-07-25 14:51:37 +0300

    solve params

--- src/app/[locale]/rooms/[id]/page.tsx
+++ src/app/[locale]/rooms/[id]/page.tsx
@@ -1,8 +1,10 @@
-import { RoomDetails } from '@/features/rooms/components/RoomDetails'
-import React from 'react'
+import { RoomDetails } from "@/features/rooms/components/RoomDetails";
+import React from "react";
 
-const page = ({ params }: { params: { id: string } }) => {
-  return <RoomDetails id={params.id} />
-}
+const page = async ({ params }: { params: { id: string } }) => {
+  const resolvedParams = await params;
+  const id = resolvedParams.id;
+  return <RoomDetails id={id} />;
+};
 
-export default page
+export default page;
