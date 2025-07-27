0dee95601feecb80b5978ba2483356bb1fa45bf7
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2025-07-27 15:06:47 +0300

    updates

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { useTranslations } from "next-intl";
 import React from "react"
+import { Button } from "@/components/ui/button";
 
 interface ErrorProps{
     error: Error;
@@ -10,7 +9,6 @@ interface ErrorProps{
 }
 
 const GlobalError = ({error, reset}: ErrorProps) =>{
-    const t = useTranslations("")
     return(
         <div className="p-20 text-center text-red-600">
             <h1>Something went wrong</h1>
